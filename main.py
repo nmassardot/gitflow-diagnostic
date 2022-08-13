cebaa3b4134643cfb750228c9b91d73fa922bb2e
Nicolás Massardo <nmassardot@uc.cl>
2022-08-13 00:01:35 -0400

    style(main): spacing between loops

--- main.py
+++ main.py
@@ -23,12 +23,14 @@ def main():
 
     elif user_input == 2:
         top_users = top_10_users(path_to_data)
+
         print("\nTop 10 users")
         for i in range(len(top_users)):
             print(f"{i+1}. {top_users[i]['username']}: {top_users[i]['tweets']}")
 
     elif user_input == 3:
         top_days = top_10_days_with_more_tweets(path_to_data)
+
         print("\nTop 10 days with more tweets")
         for i in range(len(top_days)):
             print(f"{i+1}. {top_days[i]['date']}: {top_days[i]['count']}")
